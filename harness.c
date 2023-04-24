d3853b53ab62e54b1677d75863eae02e03a28d51
saruman9 <rum.274.4@gmail.com>
2023-04-24 16:38:23 +0400

    Solve a problem with ICU

--- harness.c
+++ harness.c
@@ -11,13 +11,20 @@
 const ptrdiff_t ADDR_JNI_ONLOAD = 0x0000000000011640;
 const ptrdiff_t ADDR_PARSE_LINK = 0x000000000002F870;
 const ptrdiff_t ADDR_COPY_JNI_STRING_FROM_STR = 0x0000000000011160;
+const ptrdiff_t ICU_SQLITE_ANDROID_BINDER__INIT = 0x0000714788297F90;
+const ptrdiff_t g_ICU_VERSION = 0x00007147882A9000;
+const uint32_t ICU_VERSION = 68;
+
+static void *LIBC_SHARED = NULL;
+static void *LIBICU_BINDER = NULL;
+static void *LIBLINKPARSER = NULL;
 
 typedef struct Functions
 {
   void (*parse_link)(struct ParserResult *, struct String *);
   void (*copy_jni_string_from_str)(struct String *, const char *);
-  void (*binder_getInstance)();
-  void (*binder_init)();
+  // void (*binder_getInstance)();
+  // void (*binder_init)();
 } Functions;
 
 int afl_libfuzzer_init()
@@ -25,24 +32,41 @@ int afl_libfuzzer_init()
   return 0;
 }
 
+void set_icu_version(ptrdiff_t binder_init_addr)
+{
+  ptrdiff_t diff = g_ICU_VERSION - ICU_SQLITE_ANDROID_BINDER__INIT;
+  ptrdiff_t version_addr = binder_init_addr + diff;
+  printf("original ICU_VERSION = %X\n", *(uint32_t *)version_addr);
+  *(uint32_t *)version_addr = ICU_VERSION;
+  return;
+}
+
 struct Functions *load_functions()
 {
-  void *liblinkparser = dlopen("liblinkparser.so", RTLD_NOW | RTLD_GLOBAL);
-  void *libicu_binder = dlopen("libicuBinder.so", RTLD_NOW | RTLD_GLOBAL);
-  if (liblinkparser != NULL)
+  LIBC_SHARED = dlopen("/data/local/tmp/libc++_shared.so", RTLD_NOW | RTLD_GLOBAL);
+  LIBICU_BINDER = dlopen("/data/local/tmp/libicuBinder.so", RTLD_NOW | RTLD_GLOBAL);
+  LIBLINKPARSER = dlopen("/data/local/tmp/liblinkparser.so", RTLD_NOW | RTLD_GLOBAL);
+  if (LIBLINKPARSER != NULL && LIBC_SHARED != NULL && LIBICU_BINDER != NULL)
   {
-    int (*JNI_OnLoad)(void *, void *) = dlsym(liblinkparser, "JNI_OnLoad");
-    void (*binder_getInstance)() = dlsym(libicu_binder, "_ZN22IcuSqliteAndroidBinder11getInstanceEv");
-    void (*binder_init)() = dlsym(libicu_binder, "_ZN22IcuSqliteAndroidBinder4initEv");
-    if (JNI_OnLoad != NULL)
+    int (*JNI_OnLoad)(void *, void *) = dlsym(LIBLINKPARSER, "JNI_OnLoad");
+    // void (*binder_getInstance)() = dlsym(LIBICU_BINDER, "_ZN22IcuSqliteAndroidBinder11getInstanceEv");
+    void (*binder_init)() = dlsym(LIBICU_BINDER, "_ZN22IcuSqliteAndroidBinder4initEv");
+
+    if (JNI_OnLoad != NULL && binder_init != NULL /* && binder_getInstance != NULL */)
     {
       Dl_info jni_on_load_info;
       dladdr(JNI_OnLoad, &jni_on_load_info);
       size_t jni_on_load_addr = (size_t)jni_on_load_info.dli_saddr;
+
+      Dl_info binder_init_info;
+      dladdr(binder_init, &binder_init_info);
+      size_t binder_init_addr = (size_t)binder_init_info.dli_saddr;
+      set_icu_version(binder_init_addr);
+
       int diff_parse_link = ADDR_PARSE_LINK - ADDR_JNI_ONLOAD;
       int diff_copy_jni_string_from_str = ADDR_COPY_JNI_STRING_FROM_STR - ADDR_JNI_ONLOAD;
       size_t parse_link_addr = jni_on_load_addr + diff_parse_link;
-      size_t copy_jni_string_from_str_addr = diff_copy_jni_string_from_str + jni_on_load_addr;
+      size_t copy_jni_string_from_str_addr = jni_on_load_addr + diff_copy_jni_string_from_str;
       printf("[i] parse_link_addr: %zX\n", parse_link_addr);
       printf("[i] copy_jni_string_from_str_addr: %zX\n", copy_jni_string_from_str_addr);
       void (*parse_link)(ParserResult *, String *) = (void (*)(ParserResult *, String *))(parse_link_addr);
@@ -52,8 +76,8 @@ struct Functions *load_functions()
         Functions *functions = (Functions *)malloc(sizeof(Functions));
         functions->parse_link = parse_link;
         functions->copy_jni_string_from_str = copy_jni_string_from_str;
-        functions->binder_getInstance = binder_getInstance;
-        functions->binder_init = binder_init;
+        // functions->binder_getInstance = binder_getInstance;
+        // functions->binder_init = binder_init;
         return functions;
       }
       else
@@ -64,17 +88,22 @@ struct Functions *load_functions()
     }
     else
     {
-      perror("[-] Can't find JNI_OnLoad function (dlsym)");
+      perror("[-] Can't find symbols (dlsym)");
       return NULL;
     }
-    dlclose(liblinkparser);
-    dlclose(libicu_binder);
   }
   else
   {
     fprintf(stderr, "[-] %s\n", dlerror());
     return NULL;
   }
+}
+
+void close_libraries()
+{
+  dlclose(LIBLINKPARSER);
+  dlclose(LIBICU_BINDER);
+  dlclose(LIBC_SHARED);
 }
 
 #define TRIAGE
@@ -90,13 +119,14 @@ int main(int argc, char *argv[])
   printf("[+] Functions loaded\n");
   ParserResult *parser_result = (ParserResult *)malloc(sizeof(ParserResult));
   String *url = (String *)malloc(sizeof(String));
-  functions->binder_getInstance();
-  functions->binder_init();
-  // functions->copy_jni_string_from_str(url, argv[1]);
-  // functions->parse_link(parser_result, url);
+  // functions->binder_getInstance();
+  // functions->binder_init();
+  functions->copy_jni_string_from_str(url, argv[1]);
+  functions->parse_link(parser_result, url);
   free(parser_result);
   free(url);
   free(functions);
+  close_libraries();
   return 0;
 }
 #else  // TRIAGE
@@ -107,7 +137,7 @@ int main(int argc, char *argv[])
   Functions *functions = load_functions();
   printf("[+] Functions loaded\n");
   ParserResult *parser_result = (ParserResult *)malloc(sizeof(ParserResult));
-  String *url = (String *)malloc(sizeof(jni_string));
+  String *url = (String *)malloc(sizeof(String));
   // char *input = (char *)malloc(size + 1);
   // memcpy(input, data, size);
   // input[size] = 0;
